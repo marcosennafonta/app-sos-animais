84ad6e9b0c5e8f0dde377d54e730d2fe3882d050
gustavo quinalha <gustavo_quinalha@hotmail.com>
2016-03-16 14:20:04 -0300

    prototipo od sistema web feito com bootstrap

---
+++ teste.js
@@ -0,0 +1,42 @@
+  $(document).ready(function(){
+    $(rss).mouseover(function(){
+      $(rs).css("display", "block");
+    });
+    $(rss).mouseout(function(){
+      $(rs).css("display", "none");
+    });
+
+    $(rss2).mouseover(function(){
+      $(rs2).css("display", "block");
+    });
+    $(rss2).mouseout(function(){
+      $(rs2).css("display", "none");
+    });
+
+    $(rss3).mouseover(function(){
+      $(rs3).css("display", "block");
+    });
+    $(rss3).mouseout(function(){
+      $(rs3).css("display", "none");
+    });
+
+    $(rss4).mouseover(function(){
+      $(rs4).css("display", "block");
+    });
+    $(rss4).mouseout(function(){
+      $(rs4).css("display", "none");
+    });
+
+    $(rss5).mouseover(function(){
+      $(rs5).css("display", "block");
+    });
+    $(rss5).mouseout(function(){
+      $(rs5).css("display", "none");
+    });
+    $(rss6).mouseover(function(){
+      $(rs6).css("display", "block");
+    });
+    $(rss6).mouseout(function(){
+      $(rs6).css("display", "none");
+    });
+  });
